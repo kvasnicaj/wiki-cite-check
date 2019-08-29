a850dd4ee38a036fee65580f2f8430a8473e92cf
kvasnicaj <j.kvasnica@gmail.com>
2019-08-29 14:37:35 +0200

    db credentials

--- config/config.py
+++ config/config.py
@@ -1,7 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     DEBUG = True
     ROW_LIMIT = 10
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+    # database configuration
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
